d89c45e74394d2bce163e1bd7c76155e9ad1bc33
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-22 15:13:45 +0000

    worspace settings

--- commentary-ui/src/constants/styles.js
+++ commentary-ui/src/constants/styles.js
@@ -2,14 +2,14 @@
 const FONT = 'Roboto';
 
 const COLOURS = {
-    primary: "palevioletred",
+  primary: "palevioletred",
 
-    lightGrey: "#F8F8F8",
-    darkGrey: "#7F7F7F",
-    black: "#000000",
-    white: "#FFFFFF",
-    transBlack: "rgb(127, 127, 127, 0.25)"
-  }
+  lightGrey: "#F8F8F8",
+  darkGrey: "#7F7F7F",
+  black: "#000000",
+  white: "#FFFFFF",
+  transBlack: "rgb(127, 127, 127, 0.25)"
+}
 
 module.exports = {
  font: FONT
