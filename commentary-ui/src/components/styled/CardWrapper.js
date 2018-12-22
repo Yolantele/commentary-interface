9d745d28d2e967e5bd2249669315fd19f6d6ed7c
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-22 18:27:36 +0000

    Switches to Radium setup next to Styled Components - to consider responsive UI - automating scaling down of width

---
+++ commentary-ui/src/components/styled/CardWrapper.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CardWrapper = styled.button`
+  background: transparent;
+  border-radius: 6px;
+  border: 2px solid transparent purple;
+  color: purple;
+  font-size: 18;
+  margin: 1 1em;
+  padding: 2em 2em;
+  box-shadow: 4px 4px 4px #2222;
+`;
+
+export default CardWrapper;
--- commentary-ui/src/constants/styles.js
+++ commentary-ui/src/constants/styles.js
@@ -2,17 +2,16 @@
 const FONT = 'Roboto';
 
 const COLOURS = {
-  primary: "palevioletred",
   key: "#351254",
-
+  primary: "#ff214d",
   lightGrey: "#F8F8F8",
   darkGrey: "#7F7F7F",
   black: "#000000",
-  white: "#FFFFFF",
+  white: "#f4fff9",
   transBlack: "rgb(127, 127, 127, 0.25)"
-}
+};
 
 module.exports = {
- font: FONT,
- colours: COLOURS
+  font: FONT,
+  colours: COLOURS
 };
