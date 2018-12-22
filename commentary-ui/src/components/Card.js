164b0ce3328199ba6a7e83996a5539b3b82544bb
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-22 19:22:00 +0000

    Defines Card functional component to render comment items

---
+++ commentary-ui/src/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Radium from 'radium';
+import Styles from '../constants/styles';
+import CardWrapper from './styled/CardWrapper';
+import Texts from './styled/Texts';
+import Badge from './styled/Badge';
+
+
+const Card = (props) => {
+
+  const { badge, description, cardWidth } = props;
+  const container = [styles.container];
+
+  container.push({width: cardWidth});
+
+  
+
+  return (
+    <CardWrapper>
+      <div style={container}> 
+        <Badge>{badge}</Badge>
+        <Texts>{description}</Texts>
+      </div>
+    </CardWrapper>
+  );
+
+}
+
+
+const styles = {
+container: { 
+  display: 'flex',
+  flexDirection: 'row',
+  fontFamily: Styles.font,
+  backgroundColor: Styles.colours.pureWhite,
+}
+};
+
+
+export default Radium(Card)
