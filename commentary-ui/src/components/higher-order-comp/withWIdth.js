d0d4358974a45cc8ff1e8886fac83ba89815e784
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-22 23:09:25 +0000

    defines a higher-order-component to controll changing width depending on the screen width

---
+++ commentary-ui/src/components/higher-order-comp/withWIdth.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+
+const SMALL_WIDTH = 400 ;
+const REDUCE_IN_PROPORTION = 1.5;
+const withWidth = (MyComponent, proportion=REDUCE_IN_PROPORTION) => {
+
+
+  return class WithWidth extends Component {
+    constructor() {
+      super()
+      this.state = { width: this.windowWidth() }
+      this.handleResize = this.handleResize.bind(this)
+    }
+
+    componentDidMount() {
+      if (window) {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize()
+      }
+    }
+    
+    componentWillUnmount() {
+      if (window) window.removeEventListener('resize', this.handleResize);
+    }
+
+    handleResize() {
+      let width = this.windowWidth()
+      if (width !== this.state.width) this.setState({ width });
+    }
+    
+    windowWidth() {
+      let smallWidth = SMALL_WIDTH;
+      let innerWidth = 0;
+      let width;
+      if (window) innerWidth = window.innerWidth;
+
+      width = innerWidth / proportion;
+
+      if (width > smallWidth) return width;
+      else return smallWidth;
+    }
+
+    render() {
+      return (
+      <div style={{width: this.state.width}}>
+        <MyComponent
+          { ...this.props }
+          width={ this.state.width }
+        />
+      </div>
+      )
+    }
+  }
+}
+
+export default withWidth
